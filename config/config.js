462ee589056c545400c6a4d93c3819d4374477c7
Abid Hussain <abid.abid97@gmail.com>
2018-04-17 13:05:20 +0530

    Add config file

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 'use strict'
 
 module.exports = {
-	dbCredentials : (name = 'quiz', port = 27017, host = 'localhost') => `mongodb://${host}:${port}/${name}`,
-	privateKey : () => 'anil'
+	dbCredentials : 'mongodb://localhost:27017/quiz',
+	privateKey : 'anil'
 }
