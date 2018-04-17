f5700a2117495979f07a2a9dc3a681d959f9870c
Abid Hussain <abid.abid97@gmail.com>
2018-04-17 13:07:42 +0530

    Call connectToDb method

--- index.js
+++ index.js
@@ -2,12 +2,23 @@
 
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { exec } = require('child_process');
+const db = require('./services/db.service');
 
-const query = require('./services/db.service');
+db.connectToDB()
+	.then(db => {
+        let user = db.collection('user');
+        let taken = db.collection('taken');
+		user.createIndex( { email : 1 }, { unique: true } );
+	})
+	.catch(err => {
+		console.log('Error occurred while initializing collection.\n' + err);
+	})
 
+app.use(cookieParser());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
