6dad21728530c4da90d9e3e1f9f923e1131444a8
Abid Hussain <abid.abid97@gmail.com>
2018-04-17 13:09:09 +0530

    Add key from config

--- services/jwt.service.js
+++ services/jwt.service.js
@@ -1,13 +1,13 @@
 'use strict'
 
 const jwt = require('jsonwebtoken');
-const privateKey = require('../config/config').privateKey();
+const privateKey = require('../config/config');
 const q = require('q');
 
 const getToken = (payload, expiryTime = 30 * 12 * 60 * 60 * 60) => {
 	let defer = q.defer();
 
-	jwt.sign(payload, privateKey, { expiresIn: expiryTime }, (err, token) => {
+	jwt.sign(payload, privateKey.privateKey, { expiresIn: expiryTime }, (err, token) => {
 		if(!err) {
 			console.log('Token generated.');
 			defer.resolve(token);
@@ -24,7 +24,7 @@ const getToken = (payload, expiryTime = 30 * 12 * 60 * 60 * 60) => {
 const decodeToken = (token) => {
 	let defer = q.defer();
 
-	jwt.verify(token, privateKey, (err, decoded) => {
+	jwt.verify(token, privateKey.privateKey, (err, decoded) => {
 		if(!err) {
 			console.log('Token verified.');
 			defer.resolve(decoded);
