e8682432396c61c7c3be119554adfa2912a130ee
Abid Hussain <abid.abid97@gmail.com>
2018-04-17 13:09:47 +0530

    Add isAuthenticated middleware

---
+++ middlewares/isAuthenticated.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const jwt = require('../services/jwt.service');
+
+const isAuthenticated = (req, res, next) => {
+    let response = {
+        success : false,
+        message : undefined
+    }
+
+    const token = req.cookies.token;
+
+    if(token) {
+        jwt.decodeToken(token)
+            .then(data => {
+                req.body.data = data.email;
+                next();
+            })
+            .catch(error => {
+                response = {
+                    ...response,
+                    error,
+                    message : 'Authorized.'
+                }
+
+                res.status(200).json(response);
+            })
+    } else {
+        response = {
+            ...response,
+            message : 'Cookie undefined.'
+        }
+
+        res.status(403).json(response);
+    }
+}
+
+module.exports = {
+    isAuthenticated
+}
